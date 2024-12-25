3a613006c302e278b51b2d46c9670de5145aba60
Linus Horn <linus@linush.org>
2024-12-25 01:32:25 +0100

    convert book covers to WebP

--- server.py
+++ server.py
@@ -14,13 +14,15 @@ async def serve_book_text(book_name: str):
 
 @app.get("/book_cover")
 async def server_book_cover_img(book_name: str) -> FileResponse:
-    file_path = f"./book_covers/{book_name}.png"
-    if os.path.exists(file_path):
-        return FileResponse(file_path)
-    else:
-        # TODO: Handle 404
-        return JSONResponse({"error": "Book cover not found!"}, status_code=404)
+    webp_file_path = f"./book_covers/{book_name}.webp"
+    png_file_path = f"./book_covers/{book_name}.png"
 
+    if os.path.exists(webp_file_path):
+        return FileResponse(webp_file_path)
+    elif os.path.exists(png_file_path):
+        return FileResponse(png_file_path)
+    else:
+        return JSONResponse({"error": "No cover image found for the book"}, status_code=404)
 
 @app.get("/api/v1/list_books")
 async def get_list_of_books():
